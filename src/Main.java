74f20cc25af6ec14adbfbf842e1717b50fb0ce2f
IVAN <luka.news.2001@mail.ru>
2023-01-26 19:23:55 +0300

    Домашка - Переменные 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         task1 ();
+        task2();
 }
 public static void task1 () {
     System.out.println("Задача 1");
-    var box = 4;
-    System.out.println(box);
+    byte a = 120;
+    short b = -3021;
+    int c = 67512987;
+    long d = -783665818;
+    float e = 120.2f;
+    double f = -15021.124;
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        byte a = 67;
+        short b = 569;
+        int c = 27897;
+        long d = -159;
+        float e = 27.12F;
+        double f = 2.786;
     }
 }
